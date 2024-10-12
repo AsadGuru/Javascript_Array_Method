a96b77feedef64c6f5c149483c4c5ac7d8416be6
Mohd Asad Naseem <87413907+AsadGuru@users.noreply.github.com>
2024-10-13 01:43:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let arr = [
+    { id: 1, name: "john", age: "18", profession: "developer" },
+    { id: 2, name: "jack", age: "20", profession: "developer" },
+    { id: 3, name: "karen", age: "19", profession: "admin" },
+  ];
+  
+  function PrintDeveloperbyMap() {
+    const developers = arr.filter(person => person.profession === "developer").map(developer => developer.name);
+    console.log(developers);
+  }
+  
+  function PrintDeveloperbyForEach() {
+    arr
+      .filter(person => person.profession === "developer")
+      .forEach(developer => console.log(developer.name));
+  }
+  
+  function addData() {
+    const newPerson = { id: 4, name: "susan", age: "20", profession: "intern" };
+    arr.push(newPerson);
+    console.log(arr);
+  }
+  
+  function removeAdmin() {
+    const updatedArray = arr.filter(person => person.profession !== "admin");
+    console.log(updatedArray);
+  }
+  
+  function concatenateArray() {
+    const newArray = [
+      { id: 4, name: "emma", age: "25", profession: "manager" },
+      { id: 5, name: "mike", age: "30", profession: "developer" },
+      { id: 6, name: "martin", age: "24", profession: "developer" }
+    ];
+    const concatenatedArray = arr.concat(newArray);
+    console.log(concatenatedArray);
+  }
+  
